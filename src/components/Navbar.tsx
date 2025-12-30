d5af3dcf975177bd3e27e2fe9cca3eddb1c30cf4
Daniel Lew <lewdaniel2@gmail.com>
2025-12-29 23:36:30 -0500

    FIX: link navbar to pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,9 +9,9 @@ export default function Navbar() {
       
       <div className="hidden md:flex gap-8 text-stone-600 font-medium">
         <Link href="/services" className="hover:text-primary-700 transition-colors">Services</Link>
-        <Link href="#" className="hover:text-primary-700 transition-colors">Therapists</Link>
-        <Link href="#" className="hover:text-primary-700 transition-colors">About</Link>
-        <Link href="#" className="hover:text-primary-700 transition-colors">Contact Us</Link>
+        <Link href="/therapists" className="hover:text-primary-700 transition-colors">Therapists</Link>
+        <Link href="/about" className="hover:text-primary-700 transition-colors">About</Link>
+        <Link href="/contact" className="hover:text-primary-700 transition-colors">Contact Us</Link>
       </div>
       
       {/* <button className="bg-primary-50 text-primary-900 px-5 py-2 rounded-full font-medium hover:bg-primary-100 transition-colors border border-primary-200"> */}
